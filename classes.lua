cb8b99b476264892994b651295aa4c1b11417fac
Bekir Aşkar <yourpalbekir@gmail.com>
2022-02-14 22:48:13 +0300

    Update classes.lua

--- classes.lua
+++ classes.lua
@@ -1,37 +1,17 @@
-function new(self)
-    local c = {prototype = {}}
-    c.prototype.__index = c.prototype
-    c.prototype.constructor = c
-    return c
+template = {}
+template.__index = template
+
+function template:create()
+    local instance = {}
+    setmetatable(instance, template)
+    if instance:constructor() then
+        return instance
+    end
+    return false
 end
 
-function extends(target, base)
-    target.____super = base
-    local staticMetatable = setmetatable({__index = base}, base)
-    setmetatable(target, staticMetatable)
-    local baseMetatable = getmetatable(base)
-    if baseMetatable then
-        if type(baseMetatable.__index) == "function" then
-            staticMetatable.__index = baseMetatable.__index
-        end
-        if type(baseMetatable.__newindex) == "function" then
-            staticMetatable.__newindex = baseMetatable.__newindex
-        end
-    end
-    setmetatable(target.prototype, base.prototype)
-    if type(base.prototype.__index) == "function" then
-        target.prototype.__index = base.prototype.__index
-    end
-    if type(base.prototype.__newindex) == "function" then
-        target.prototype.__newindex = base.prototype.__newindex
-    end
-    if type(base.prototype.__tostring) == "function" then
-        target.prototype.__tostring = base.prototype.__tostring
-    end
+function template:constructor()
+    
 end
 
-function load(target, ...)
-    local instance = setmetatable({}, target.prototype)
-    instance:____constructor(...)
-    return instance
-end
+template:create()
